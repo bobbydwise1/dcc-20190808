f981ea350008d10a47aecea5bcd266d057c50f8b
Robert Lee <bobbydwise1@hotmail.com>
2019-08-08 22:27:28 -0700

    Create square bracket pair test.

--- scripts.js
+++ scripts.js
@@ -10,26 +10,38 @@ For example, given the string "([])[]({})", you should return true.
 Given the string "([)]" or "((()", you should return false.
 */
 
-const testing = (yourString) => {
-  let regex1 = /[\[][^\[]*[\]]/
-  let regex2 = /[\(][^\(]*[\)]/
-  let regex3 = /[\{][^\{]*[\}]/
-  let condition1 = yourString.match(regex1);
-  console.log('1: ',condition1)
-  let condition2 = yourString.match(regex2);
-  console.log('2: ',condition2)
-  let condition3 = yourString.match(regex3);
-  console.log('3: ',condition3)
+// const testing = (yourString) => {
+//   let regex1 = /[\[][^\[]*[\]]/
+//   let regex2 = /[\(][^\(]*[\)]/
+//   let regex3 = /[\{][^\{]*[\}]/
+//   let condition1 = yourString.match(regex1);
+//   console.log('1: ',condition1)
+//   let condition2 = yourString.match(regex2);
+//   console.log('2: ',condition2)
+//   let condition3 = yourString.match(regex3);
+//   console.log('3: ',condition3)
+//
+// }
 
+const testingSquare = (yourString) => {
+  let regex1 = /[\[]/
+  let regex2 = /[\]]/
+  let matches1 = yourString.match(regex1);
+  let matches2 = yourString.match(regex2);
+  if ((matches1 === null) && (matches2 === null)) {return 1}
+  if ((matches1.length == matches2.length) && (yourString.search(regex1) < yourString.search(regex2))) {return 1;}
+  return 0;
 }
 
 let test1 = "([])[]({})"; //true
 let test2 = "([)]"; //false
 let test3 = "((()"; //false
+let test4 = "][]"
 
-console.log(testing(test1));
-console.log(testing(test2));
-console.log(testing(test3));
+console.log(testingSquare(test1));
+console.log(testingSquare(test2));
+console.log(testingSquare(test3));
+console.log(testingSquare(test4));
 
 $(document).ready(function() {
     $('#output-section-1').text(1);
